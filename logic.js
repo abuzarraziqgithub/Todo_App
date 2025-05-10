942e92227ae0d277f5d152e859f14d3631f764cb
abuzarraziqgithub <abuzarraziqo@gmail.com>
2025-05-10 12:53:14 +0500

    project finished

--- logic.js
+++ logic.js
@@ -1,32 +1,47 @@
-// "use strict";
+addEventListener("DOMContentLoaded", () => {
+  const inputData = document.getElementById("todoInput");
+  const addBtn = document.getElementById("addBtn");
+  const uList = document.querySelector(".ulList");
+  const deleteBtn = document.getElementById("deleteBtn");
 
-const inputData = document.getElementById("todoInput");
-const addBtn = document.getElementById("addBtn");
-const uList = document.querySelector(".ulList");
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-let tasks = [];
+  tasks.forEach((task) => renderTask(task));
 
-addBtn.addEventListener("click", function () {
-  const inputText = inputData.value.trim();
-  if (inputData.value === "") return;
+  addBtn.addEventListener("click", function () {
+    const inputText = inputData.value.trim();
+    if (inputData.value === "") return;
 
-  const newTask = {
-    id: Date.now(),
-    text: inputText,
-    completed: false,
-  };
+    const newTask = {
+      id: Date.now(),
+      text: inputText,
+      completed: false,
+    };
+    tasks.push(newTask);
+    saveTasks();
+    renderTask(newTask);
+    inputData.value = "";
+  });
 
-  tasks.push(newTask);
-  renderTask();
-  inputData.value = "";
+  function renderTask(task) {
+    const li = document.createElement("li");
+    li.setAttribute("data-id", task.id);
+    li.innerHTML = `
+    <span>${task.text}</span>
+    <button>-</button>
+    `;
+    li.querySelector("button").addEventListener("click", (e) => {
+      e.stopPropagation();
+
+      tasks = tasks.filter((t) => t.id !== task.id);
+      li.remove();
+      saveTasks();
+    });
+
+    uList.appendChild(li);
+  }
+
+  function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
 });
-
-function renderTask(task) {
-  const li = document.createElement("li");
-  // li.innerHTML = `<button type="button" id="deleteBtn">-</button>`;
-  uList.appendChild(li);
-}
-
-function saveTasks() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-}
